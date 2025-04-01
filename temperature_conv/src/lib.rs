584b95afc723a404d98d5055c0ba2f594dbe4a75
hiombima <ombimahillary52@gmail.com>
2025-04-01 23:45:23 +0300

    refine fahrenheit_to_celsius function to round result to four decimal places

--- temperature_conv/src/lib.rs
+++ temperature_conv/src/lib.rs
@@ -1,5 +1,6 @@
 pub fn fahrenheit_to_celsius(f: f64) -> f64 {
-    (f - 32.0) * 5.0 / 9.0
+   let c = (f - 32.0) * 5.0 / 9.0;
+   (c * 10000.0).round() / 10000.0
 }
 
 pub fn celsius_to_fahrenheit(c: f64) -> f64 {
