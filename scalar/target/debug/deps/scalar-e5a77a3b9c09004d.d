cd0a99778236f4d04e3d25541b6aaa3fa0ca5106
hiombima <ombimahillary52@gmail.com>
2025-04-01 22:47:24 +0300

    remove generated target files from repository

--- scalar/target/debug/deps/scalar-e5a77a3b9c09004d.d
+++
@@ -1,5 +0,0 @@
-/home/hiombima/piscine-rust/scalar/target/debug/deps/scalar-e5a77a3b9c09004d: src/lib.rs
-
-/home/hiombima/piscine-rust/scalar/target/debug/deps/scalar-e5a77a3b9c09004d.d: src/lib.rs
-
-src/lib.rs:
