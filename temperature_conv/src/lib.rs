c79717addefe81e6c709421c08fd16224169f27a
hiombima <ombimahillary52@gmail.com>
2025-04-01 23:46:43 +0300

    remove unused test module from temperature_conv package

--- temperature_conv/src/lib.rs
+++ temperature_conv/src/lib.rs
@@ -6,18 +6,3 @@ pub fn fahrenheit_to_celsius(f: f64) -> f64 {
 pub fn celsius_to_fahrenheit(c: f64) -> f64 {
     (c * 9.0) / 5.0 + 32.0
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_fahrenheit_to_celsius() {
-        assert_eq!(fahrenheit_to_celsius(-459.67), -273.15);
-    }
-
-    #[test]
-    fn test_celsius_to_fahrenheit() {
-        assert_eq!(celsius_to_fahrenheit(0.0), 32.0);
-    }
-}
