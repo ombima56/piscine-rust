debf565a48904de2c523439a373bba12a9869f4a
hiombima <ombimahillary52@gmail.com>
2025-04-01 22:42:00 +0300

    initialize scalar package with basic arithmetic functions and tests

---
+++ scalar/src/lib.rs
@@ -0,0 +1,54 @@
+pub fn sum(a: u8, b: u8) -> u8 {
+    a + b
+    
+}
+
+pub fn diff(a: i16, b: i16) -> i16 {
+    a - b
+}
+
+pub fn pro(a: i8, b: i8) -> i8 {
+    a * b
+}
+
+pub fn quo(a: f32, b: f32) -> f32 {
+    a / b
+
+}
+
+pub fn rem(a: f32, b: f32) -> f32 {
+   a % b
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_sum() {
+        assert_eq!(sum(100, 30), 130);
+        assert_eq!(sum(30, 150), 180); // Clamped at 180
+    }
+
+    #[test]
+    fn test_diff() {
+        assert_eq!(diff(234, 2), 232);
+        assert_eq!(diff(10, 20), -10); // Clamped at 0
+    }
+
+    #[test]
+    fn test_pro() {
+        assert_eq!(pro(10, 10), 100);
+        assert_eq!(pro(11, 2), 22); // Clamped at 22
+    }
+
+    #[test]
+    fn test_quo() {
+        assert_eq!(quo(100.0, 2.0), 50.0);
+    }
+
+    #[test]
+    fn test_rem() {
+        assert_eq!(rem(10.0, 3.0), 1.0);
+    }
+}
---
+++ scalar/target/debug/deps/scalar-8d70226be9d45d92.d
@@ -0,0 +1,7 @@
+/home/hiombima/piscine-rust/scalar/target/debug/deps/libscalar-8d70226be9d45d92.rmeta: src/lib.rs
+
+/home/hiombima/piscine-rust/scalar/target/debug/deps/libscalar-8d70226be9d45d92.rlib: src/lib.rs
+
+/home/hiombima/piscine-rust/scalar/target/debug/deps/scalar-8d70226be9d45d92.d: src/lib.rs
+
+src/lib.rs:
