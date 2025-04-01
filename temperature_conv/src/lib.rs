79572d554cbf48688cb934b8a8ede9a067afc657
hiombima <ombimahillary52@gmail.com>
2025-04-01 23:53:19 +0300

    simplify fahrenheit_to_celsius function by removing unnecessary rounding

--- temperature_conv/src/lib.rs
+++ temperature_conv/src/lib.rs
@@ -1,6 +1,5 @@
 pub fn fahrenheit_to_celsius(f: f64) -> f64 {
-   let c = (f - 32.0) * 5.0 / 9.0;
-   (c * 10000.0).round() / 10000.0
+    (f - 32.0) * 5.0 / 9.0
 }
 
 pub fn celsius_to_fahrenheit(c: f64) -> f64 {
