e8bcb2d84a45d2933e9db305dd659fbb83077b30
hiombima <ombimahillary52@gmail.com>
2025-04-01 23:24:11 +0300

    initialize temperature_conv package with conversion functions and tests

---
+++ temperature_conv/target/debug/deps/temperature_conv-4eb04a7507869acf.d
@@ -0,0 +1,7 @@
+/home/hiombima/piscine-rust/temperature_conv/target/debug/deps/libtemperature_conv-4eb04a7507869acf.rmeta: src/lib.rs
+
+/home/hiombima/piscine-rust/temperature_conv/target/debug/deps/libtemperature_conv-4eb04a7507869acf.rlib: src/lib.rs
+
+/home/hiombima/piscine-rust/temperature_conv/target/debug/deps/temperature_conv-4eb04a7507869acf.d: src/lib.rs
+
+src/lib.rs:
---
+++ temperature_conv/target/debug/deps/temperature_conv-91ad9cc1f8324a7c.d
@@ -0,0 +1,5 @@
+/home/hiombima/piscine-rust/temperature_conv/target/debug/deps/temperature_conv-91ad9cc1f8324a7c: src/lib.rs
+
+/home/hiombima/piscine-rust/temperature_conv/target/debug/deps/temperature_conv-91ad9cc1f8324a7c.d: src/lib.rs
+
+src/lib.rs:
