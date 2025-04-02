ef85114cfe11c6a669c4bc676c363acda504fad6
hiombima <ombimahillary52@gmail.com>
2025-04-02 10:34:36 +0300

    add matrix_transposition package with matrix struct and transpose function, including unit tests

---
+++ matrix_transposition/src/lib.rs
@@ -0,0 +1,22 @@
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct Matrix((i32, i32), (i32, i32));
+
+pub fn transpose(m: Matrix) -> Matrix {
+    let Matrix((a, b), (c, d)) = m;
+    
+    Matrix((a, c), (b, d))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn test_transpose() {
+        assert_eq!(transpose(Matrix((1, 2), (3, 4))), Matrix((1, 3), (2, 4)));
+        assert_eq!(transpose(Matrix((5, 6), (7, 8))), Matrix((5, 7), (6, 8)));
+        
+        let matrix = Matrix((1, 2), (3, 4));
+        assert_eq!(transpose(transpose(matrix.clone())), matrix);
+    }
+}
