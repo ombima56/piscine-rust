091c0eee51309906e70f37f047ce40508e843e35
hiombima <ombimahillary52@gmail.com>
2025-04-03 15:15:50 +0300

    fix arrange_it package: enhance sorting logic in arrange_phrase function to correctly handle multiple digits

--- arrange_it/src/lib.rs
+++ arrange_it/src/lib.rs
@@ -1,13 +1,15 @@
 pub fn arrange_phrase(phrase: &str) -> String {
     let mut words: Vec<&str> = phrase.split_whitespace().collect();
-    
+
     words.sort_by_key(|word| {
         word.chars()
-            .find(|c| c.is_digit(10))
-            .map(|c| c.to_digit(10).unwrap())
+            .filter(|c| c.is_digit(10))
+            .collect::<String>()
+            
+            .parse::<usize>()
             .unwrap_or(0)
     });
-    
+
     words.join(" ")
 }
 
@@ -22,3 +24,4 @@ mod tests {
         assert_eq!(arrange_phrase(input), expected);
     }
 }
+
