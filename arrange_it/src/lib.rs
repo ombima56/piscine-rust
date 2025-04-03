056b27d64a234c1ada960f7448cdf4b7dea2558b
hiombima <ombimahillary52@gmail.com>
2025-04-03 15:12:07 +0300

    fix arrange_it package: improve sorting logic in arrange_phrase function to handle cases without digits

--- arrange_it/src/lib.rs
+++ arrange_it/src/lib.rs
@@ -1,6 +1,13 @@
 pub fn arrange_phrase(phrase: &str) -> String {
     let mut words: Vec<&str> = phrase.split_whitespace().collect();
-    words.sort_by_key(|word| word.chars().find(|c| c.is_digit(10)).unwrap());
+    
+    words.sort_by_key(|word| {
+        word.chars()
+            .find(|c| c.is_digit(10))
+            .map(|c| c.to_digit(10).unwrap())
+            .unwrap_or(0)
+    });
+    
     words.join(" ")
 }
 
