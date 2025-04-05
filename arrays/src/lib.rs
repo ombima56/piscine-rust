0b1b19fa29d4314a2346b12082d45287f9fac6dd
ombima56 <ombimahillary6@gmail.com>
2025-04-05 22:49:14 +0300

    feat: add initial implementation of arrays package with sum function and array creation

---
+++ arrays/src/lib.rs
@@ -0,0 +1,18 @@
+pub fn sum(a: &[i32]) -> i32 {
+    a.iter().sum()
+}
+
+pub fn thirtytwo_tens() -> [i32; 32] {
+    [10; 32]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
