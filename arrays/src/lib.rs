15a261e59a27864fac8d96fc6cd269cd4b1fa540
hiombima <ombimahillary52@gmail.com>
2025-04-07 09:40:34 +0300

    refactor: remove unused test module from arrays implementation

--- arrays/src/lib.rs
+++ arrays/src/lib.rs
@@ -5,14 +5,3 @@ pub fn sum(a: &[i32]) -> i32 {
 pub fn thirtytwo_tens() -> [i32; 32] {
     [10; 32]
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
