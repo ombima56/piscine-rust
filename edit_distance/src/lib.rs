71003be7a393ef0511e877b138277f0491093199
hiombima <ombimahillary52@gmail.com>
2025-04-07 09:40:05 +0300

    refactor: remove unused test module from edit_distance implementation

--- edit_distance/src/lib.rs
+++ edit_distance/src/lib.rs
@@ -33,13 +33,3 @@ pub fn edit_distance(source: &str, target: &str) -> usize {
     dp[m][n]
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
