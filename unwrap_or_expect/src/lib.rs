47314e30bae667d83c637f2f9c10063e935b5e94
hiombima <ombimahillary52@gmail.com>
2025-04-08 16:02:48 +0300

    refactor: simplify fetch_data function by using unwrap for Security::Unknown case

--- unwrap_or_expect/src/lib.rs
+++ unwrap_or_expect/src/lib.rs
@@ -8,22 +8,23 @@ pub enum Security {
 
 pub fn fetch_data(server: Result<&str, &str>, security_level: Security) -> String {
     match security_level {
-        Security::Unknown => match server {
-            Ok(url) => url.to_string(),
-            Err(_) => panic!(),
-        },
+        Security::Unknown => server.unwrap().to_string(),
+
         Security::Message => match server {
             Ok(url) => url.to_string(),
             Err(_) => panic!("ERROR: program stops"),
         },
+
         Security::Warning => match server {
             Ok(url) => url.to_string(),
             Err(_) => "WARNING: check the server".to_string(),
         },
+
         Security::NotFound => match server {
             Ok(url) => url.to_string(),
             Err(msg) => format!("Not found: {}", msg),
         },
+
         Security::UnexpectedUrl => match server {
             Ok(url) => panic!("{}", url),
             Err(msg) => msg.to_string(),
