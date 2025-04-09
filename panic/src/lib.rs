c7713e53bd891c69b211c0c45b17b43dd89a9026
hiombima <ombimahillary52@gmail.com>
2025-04-09 12:43:59 +0300

    feat: add panic package with file handling functionality and tests

---
+++ panic/src/lib.rs
@@ -0,0 +1,25 @@
+use std::fs::File;
+
+pub fn open_file(s: &str) -> File {
+    File::open(s).unwrap_or_else(|_| {
+        println!("File not found: {}, creating it...", s);
+        File::create(s).unwrap()
+    })
+}
+
+#[test]
+fn test_open_file() {
+    let filename = "not_here.txt";
+
+    if std::fs::metadata(filename).is_ok() {
+        std::fs::remove_file(filename).unwrap();
+    }
+
+    open_file(filename);
+
+    assert!(std::fs::metadata(filename).is_ok());
+
+    std::fs::remove_file(filename).unwrap();
+}
+
+
