116ee01099869768dfaac7bc3fb085b8d63a7a25
ombima56 <ombimahillary6@gmail.com>
2025-04-10 21:58:49 +0300

    refactor: simplify imports and improve middle_day function logic

--- middle_day/src/lib.rs
+++ middle_day/src/lib.rs
@@ -1,14 +1,12 @@
-use chrono::{Datelike, NaiveDate, Weekday as wd};
+pub use chrono::Weekday as wd;
+
+use chrono::{Datelike, NaiveDate};
 
 pub fn middle_day(year: i32) -> Option<wd> {
     let is_leap = NaiveDate::from_ymd_opt(year, 2, 29).is_some();
-
     if is_leap {
         return None;
     }
-
     let middle_date = NaiveDate::from_yo_opt(year, 183)?;
-
     Some(middle_date.weekday())
 }
-
