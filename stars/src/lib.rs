93c25cc1c99a0ae6944880c78667c8899ec490b1
ombima56 <ombimahillary6@gmail.com>
2025-04-11 07:18:04 +0300

    feat: add calculation for exponential number of asterisks in stars function

--- stars/src/lib.rs
+++ stars/src/lib.rs
@@ -1,5 +1,6 @@
 pub fn stars(n: u32) -> String {
     let mut result = String::new();
+    // Calculates 2^n (2 raised to the power of n), and stores it in num_stars.
     let num_stars = 2u32.pow(n);
     for _ in 0..num_stars {
         result.push('*');
