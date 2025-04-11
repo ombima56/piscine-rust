028fe327a210e92e56ea9e182b5d4b2bd0483187
ombima56 <ombimahillary6@gmail.com>
2025-04-11 08:41:52 +0300

    feat: add score function to calculate character scores and include unit tests

---
+++ scores/src/lib.rs
@@ -0,0 +1,27 @@
+pub fn score(s: &str) -> u64 {
+    s.chars()
+        .map(|c| match c.to_ascii_uppercase() {
+            'A' | 'E' | 'I' | 'O' | 'U' | 'L' | 'N' | 'R' | 'S' | 'T' => 1,
+            'D' | 'G' => 2,
+            'B' | 'C' | 'M' | 'P' => 3,
+            'F' | 'H' | 'V' | 'W' | 'Y' => 4,
+            'K' => 5,
+            'J' | 'X' => 8,
+            'Q' | 'Z' => 10,
+            _ => 0,
+        })
+        .sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn best_score() {
+        assert_eq!(score("a"), 1);
+        assert_eq!(score("ã ê Á?"), 0);
+        assert_eq!(score("ThiS is A Test"), 14);
+        assert_eq!(score("ThiS is A Test!"), 14);
+    }
+}
