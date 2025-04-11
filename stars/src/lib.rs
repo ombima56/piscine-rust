2ce813959cc8febd8d5312a852842d86e38e6e0f
ombima56 <ombimahillary6@gmail.com>
2025-04-11 07:14:27 +0300

    feat: update stars function to generate exponential number of asterisks and adjust unit tests

--- stars/src/lib.rs
+++ stars/src/lib.rs
@@ -1,6 +1,7 @@
 pub fn stars(n: u32) -> String {
     let mut result = String::new();
-    for _ in 0..n {
+    let num_stars = 2u32.pow(n);
+    for _ in 0..num_stars {
         result.push('*');
     }
     result
@@ -12,8 +13,8 @@ mod tests {
 
     #[test]
     fn test_stars() {
-        assert_eq!(stars(1), "*");
-        assert_eq!(stars(4), "****");
-        assert_eq!(stars(5), "*****");
+        assert_eq!(stars(1), "**");
+        assert_eq!(stars(4), "****************");
+        assert_eq!(stars(5), "********************************");
     }
 }
