b986e20acbd32749ca3bcd52417045725b26846e
ombima56 <ombimahillary6@gmail.com>
2025-04-11 18:15:05 +0300

    feat: add main function to demonstrate number spelling functionality

---
+++ spelling/src/main.rs
@@ -0,0 +1,11 @@
+use spelling::*;
+
+fn main() {
+    println!("{}", spell(348));
+    println!("{}", spell(9996));
+}
+
+/*  
+    three hundred forty-eight
+    nine thousand nine hundred ninety-six
+*/
