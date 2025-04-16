3b11d5109e8f6ac3c329bed8d3543749febd3d70
hiombima <ombimahillary52@gmail.com>
2025-04-16 15:45:58 +0300

    refactor: streamline Worker struct and enhance Logger methods for improved message handling

--- ref_cell/src/lib.rs
+++ ref_cell/src/lib.rs
@@ -1,50 +1,42 @@
+pub use std::collections::HashMap;
+pub use std::cell::RefCell;
+pub use std::rc::Rc;
+
+pub use messenger::*;
+
 pub mod messenger;
 
-use std::cell::RefCell;
-use std::collections::HashMap;
-use std::rc::Rc;
-
-use messenger::Logger;
-
+#[derive(Debug)]
 pub struct Worker {
-    pub track_value: Rc<String>,
+    pub track_value: Rc<usize>,
     pub mapped_messages: RefCell<HashMap<String, String>>,
     pub all_messages: RefCell<Vec<String>>,
 }
 
 impl Worker {
-    pub fn new(init_value: usize) -> Self {
+pub    fn new(num:usize) -> Worker {
         Worker {
-            track_value: Rc::new(init_value.to_string()),
+            track_value: Rc::new(num),
             mapped_messages: RefCell::new(HashMap::new()),
             all_messages: RefCell::new(Vec::new()),
         }
     }
+  
 }
 
 impl Logger for Worker {
-    fn info(&self, msg: &str) {
-        self.mapped_messages
-            .borrow_mut()
-            .insert("Info".to_string(), msg.to_string());
-        self.all_messages.borrow_mut().push(format!("Info: {}", msg));
+   fn info(&self, msg: &str) {
+        self.mapped_messages.borrow_mut().insert("Info".to_owned(),msg.to_owned().drain(6..msg.len()).collect());
+        self.all_messages.borrow_mut().push(format!("{}",msg.to_owned()));
     }
-
-    fn warning(&self, msg: &str) {
-        self.mapped_messages
-            .borrow_mut()
-            .insert("Warning".to_string(), msg.to_string());
-        self.all_messages
-            .borrow_mut()
-            .push(format!("Warning: {}", msg));
-    }
-
     fn error(&self, msg: &str) {
-        self.mapped_messages
-            .borrow_mut()
-            .insert("Error".to_string(), msg.to_string());
-        self.all_messages
-            .borrow_mut()
-            .push(format!("Error: {}", msg));
+        self.mapped_messages.borrow_mut().insert("Error".to_owned(),msg.to_owned().drain(7..msg.len()).collect());
+        self.all_messages.borrow_mut().push(format!("{}",msg.to_owned()));
+
+    }
+    fn warning(&self, msg: &str) {
+        self.mapped_messages.borrow_mut().insert("Warning".to_owned(),msg.to_owned().drain(9..msg.len()).collect());
+        self.all_messages.borrow_mut().push(format!("{}",msg.to_owned()));
+
     }
 }
