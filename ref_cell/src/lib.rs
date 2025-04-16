f1870e9f268fd11d1a034448e9d34e689526a541
hiombima <ombimahillary52@gmail.com>
2025-04-16 15:04:17 +0300

    refactor: remove unused test module from lib.rs

--- ref_cell/src/lib.rs
+++ ref_cell/src/lib.rs
@@ -48,14 +48,3 @@ impl Logger for Worker {
             .push(format!("Error: {}", msg));
     }
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
