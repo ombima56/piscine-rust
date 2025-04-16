f1a95f2354f4228b079a331a4b1bfafd66685070
hiombima <ombimahillary52@gmail.com>
2025-04-16 14:26:16 +0300

    feat: add Tracker struct with Logger trait for quota management

---
+++ ref_cell/src/messenger.rs
@@ -0,0 +1,42 @@
+use std::rc::Rc;
+
+pub trait Logger {
+    fn warning(&self, msg: &str);
+    fn info(&self, msg: &str);
+    fn error(&self, msg: &str);
+}
+
+pub struct Tracker<'a, T: Logger> {
+    logger: &'a T,
+    max: usize,
+    // Remove the unused 'value' field
+}
+
+impl<'a, T: Logger> Tracker<'a, T> {
+    pub fn new(logger: &'a T, max: usize) -> Self {
+        Self {
+            logger,
+            max,
+        }
+    }
+
+    pub fn set_value(&self, val: &Rc<String>) {
+        let count = Rc::strong_count(val);
+        let percent = count * 100 / self.max;
+
+        if percent >= 100 {
+            self.logger.error("Error: you are over your quota!");
+        } else if percent >= 70 {
+            self.logger.warning(&format!(
+                "Warning: you have used up over {}% of your quota! Proceeds with precaution",
+                percent
+            ));
+        }
+    }
+
+    pub fn peek(&self, val: &Rc<String>) {
+        let count = Rc::strong_count(val);
+        let percent = count * 100 / self.max;
+        self.logger.info(&format!("Info: you are using up to {}% of your quota", percent));
+    }
+}
