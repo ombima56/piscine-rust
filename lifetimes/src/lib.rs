8c44fa7f77d5a367efc85580b2bcbe64ae653d16
ombima56 <ombimahillary6@gmail.com>
2025-04-23 23:47:38 +0300

    feat: implement Person struct with constructor and tests

---
+++ lifetimes/src/lib.rs
@@ -0,0 +1,33 @@
+#[derive(Debug)]
+pub struct Person<'a>{
+	pub name: &'a str,
+	pub age: u8,
+}
+
+impl<'a> Person<'a> {
+	pub fn new(name: &'a str) -> Person<'a> {
+            Person {
+            name,
+            age: 0,
+        }       
+	}
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    
+    #[test]
+    fn test_person() {
+        let person = Person::new("Leo");
+        assert_eq!(person.name, "Leo");
+        assert_eq!(person.age, 0);
+    }
+    #[test]
+    fn test_person_age() {
+        let mut person = Person::new("Leo");
+        person.age = 25;
+        assert_eq!(person.age, 25);
+    }
+}
