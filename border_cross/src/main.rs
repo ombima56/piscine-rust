1f45560ab33eb3a70eeb86f36755dabe5f816352
ombima56 <ombimahillary6@gmail.com>
2025-04-24 00:22:44 +0300

    feat: add main function to demonstrate vehicle usage with Car and Truck structs

---
+++ border_cross/src/main.rs
@@ -0,0 +1,23 @@
+use border_cross::Vehicle;
+use border_cross::Car;
+use border_cross::Truck;
+use border_cross::all_models;
+
+fn main() {
+    let vehicles: Vec<&dyn Vehicle> = vec![
+        &Car {
+            plate_nbr: "A3D5C7",
+            model: "Model 3",
+            horse_power: 325,
+            year: 2010,
+        },
+        &Truck {
+            plate_nbr: "V3D5CT",
+            model: "Ranger",
+            horse_power: 325,
+            year: 2010,
+            load_tons: 40,
+        },
+    ];
+    println!("{:?}", all_models(vehicles));
+}
