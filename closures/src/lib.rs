05e412880fe78cd9fe7af289b23f68114daf98cc
hiombima <ombimahillary52@gmail.com>
2025-04-25 13:30:13 +0300

    fix(lib): remove 0 a even number

--- closures/src/lib.rs
+++ closures/src/lib.rs
@@ -1,7 +1,7 @@
 pub fn first_fifty_even_square() -> Vec<i32> {
     let square = |x: i32| x * x;
 
-    (0..).filter(|x| x % 2 == 0)
+    (1..).filter(|x| x % 2 == 0)
         .take(50)
         .map(square)
         .collect()
