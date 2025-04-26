1ae63398c4770a3ace081ffbd3cc00da6e30f6bc
ombima56 <ombimahillary6@gmail.com>
2025-04-26 23:15:16 +0300

    refactor(lib): simplify next method logic in ThrowObject iterator

--- project_motion/src/lib.rs
+++ project_motion/src/lib.rs
@@ -29,28 +29,14 @@ impl Iterator for ThrowObject {
     type Item = ThrowObject;
 
     fn next(&mut self) -> Option<Self::Item> {
-        const GRAVITY: f32 = 9.8;
-
-        // If it's already on or below the ground, return None
-        if self.actual_position.y <= 0.0 && self.time != 0.0 {
-            return None;
-        }
-
         self.time += 1.0;
-
-        // Calculate new velocity after 1s
-        let new_velocity_x = self.init_velocity.x;
-        let new_velocity_y = self.init_velocity.y - GRAVITY * self.time;
-
-        // Calculate new position after 1s
-        let new_position_x = self.init_position.x + self.init_velocity.x * self.time;
-        let new_position_y = self.init_position.y + self.init_velocity.y * self.time - 0.5 * GRAVITY * self.time * self.time;
-
-        self.actual_position.x = new_position_x;
-        self.actual_position.y = new_position_y;
-        self.actual_velocity.x = new_velocity_x;
-        self.actual_velocity.y = new_velocity_y;
-
+    
+        self.actual_velocity.x = self.init_velocity.x;
+        self.actual_velocity.y = self.init_velocity.y - 9.8 * self.time;
+        
+        self.actual_position.x = self.init_position.x + self.init_velocity.x * self.time;
+        self.actual_position.y = self.init_position.y + self.init_velocity.y * self.time - 0.5 * 9.8 * self.time * self.time;
+        
         if self.actual_position.y <= 0.0 {
             None
         } else {
