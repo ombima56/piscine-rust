b441061226b09a58432a6b87c3ceaa731d7363ac
ombima56 <ombimahillary6@gmail.com>
2025-04-26 23:08:35 +0300

    refactor(lib): improve next method logic in ThrowObject iterator

--- project_motion/src/lib.rs
+++ project_motion/src/lib.rs
@@ -31,21 +31,30 @@ impl Iterator for ThrowObject {
     fn next(&mut self) -> Option<Self::Item> {
         const GRAVITY: f32 = 9.8;
 
-        if self.time > 0.0 && self.actual_position.y <= 0.0 {
+        // If it's already on or below the ground, return None
+        if self.actual_position.y <= 0.0 && self.time != 0.0 {
             return None;
         }
 
         self.time += 1.0;
 
-        let new_x = self.init_position.x + self.init_velocity.x * self.time;
-        let new_y = self.init_position.y + self.init_velocity.y * self.time - 0.5 * GRAVITY * self.time * self.time;
-        let new_vx = self.init_velocity.x;
-        let new_vy = self.init_velocity.y - GRAVITY * self.time;
+        // Calculate new velocity after 1s
+        let new_velocity_x = self.init_velocity.x;
+        let new_velocity_y = self.init_velocity.y - GRAVITY * self.time;
 
-        self.actual_position = Object { x: new_x, y: new_y };
-        self.actual_velocity = Object { x: new_vx, y: new_vy };
+        // Calculate new position after 1s
+        let new_position_x = self.init_position.x + self.init_velocity.x * self.time;
+        let new_position_y = self.init_position.y + self.init_velocity.y * self.time - 0.5 * GRAVITY * self.time * self.time;
 
-        Some(self.clone())
+        self.actual_position.x = new_position_x;
+        self.actual_position.y = new_position_y;
+        self.actual_velocity.x = new_velocity_x;
+        self.actual_velocity.y = new_velocity_y;
+
+        if self.actual_position.y <= 0.0 {
+            None
+        } else {
+            Some(self.clone())
+        }
     }
 }
-
